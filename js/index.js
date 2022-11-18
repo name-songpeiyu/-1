a6a470800c3484c5aca254ed880fb8e483598ac7
宋培雨 <2111467955@qq.com.com>
2022-11-18 12:19:54 +0800

    '完'

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-var a = '';
+var a = null;
+var b = 375;
+function fu() {
+    clearInterval(a);
+    a = setInterval(function () {
+        var html_ = document.getElementsByTagName('html')[0];
+        var w = document.clientWidth || document.body.clientWidth;
+        html_.style.fontSize = (w / b) * 10 + ('px')
+    }, 20)
+
+}
+fu();
